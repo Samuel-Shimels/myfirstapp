8ec9605a9f178dbaf7c70814d9c9e0c871b659e6
samuel s <samuelshimels3@gmail.com>
2024-10-24 11:22:12 +0300

    update with branch

--- myfirstapp/src/App.js
+++ myfirstapp/src/App.js
@@ -4,7 +4,7 @@ function App(props) {
   return (
     <div>
       <h1>Hello, I'm Samuel!</h1>
-      <h2>It is {currDate.toLocaleDateString()} and time is  {currDate.toLocaleTimeString()}.</h2>
+      <h2>This is my first react app that display current date time from branch. It is {currDate.toLocaleDateString()} and time is  {currDate.toLocaleTimeString()}.</h2>
     </div>
   );
 }
